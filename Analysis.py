b7e1eb66dd8533230fef2d2a9bff0ee875dc5a97
Victor Lopez <lopezmvictor98@gmail.com>
2026-05-10 18:05:15 -0500

    Minor update to analysis script
    Updated data display options (head, tail, info).
    Had issues pushing from VS Code, so uploaded directly via GitHub.

--- Analysis.py
+++ Analysis.py
@@ -2,4 +2,14 @@ import pandas as pd
 
 df = pd.read_csv('data/sales dataset.csv')
 
-print(df)
+#   Option 1: Allows the full Data
+#print(df)
+
+# Option 2: This allows to show the first few rows [LIMITS]
+#print(df.head())
+
+# Option 3: This allows to show the last few rows [LIMITS]
+#print(df.tail())
+
+# This shows the structure of the Data [Colum. Names/Data Type/Missing Values]
+#print(df.info())
